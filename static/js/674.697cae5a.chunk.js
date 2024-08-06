5069f48611ac6a9851a4e0b1eef0d2f886816b6a
devromarc <69194960+devromarc@users.noreply.github.com>
2024-08-06 19:37:41 +0000

    Deploying to gh-pages from @ devromarc/goit-react-hw-08-phonebook@5cdd6a71237fd0559c87bdc2b3c4abd78d18bb50 🚀

---
+++ static/js/674.697cae5a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[674],{674:(e,s,c)=>{c.r(s),c.d(s,{default:()=>a});c(791);const r="HomePage_container__uNu-e";var l=c(50),n=c(173),t=c(329);const a=()=>{const{isLoggedIn:e,user:s}=(0,n.a)();return(0,t.jsxs)(l.B6,{children:[(0,t.jsx)(l.ql,{children:(0,t.jsx)("title",{children:"Home Page"})}),(0,t.jsx)("div",{className:r,children:e?(0,t.jsxs)("h1",{children:["Welcome ",s.name]}):(0,t.jsx)("h1",{children:"Welcome User!"})})]})}}}]);
+//# sourceMappingURL=674.697cae5a.chunk.js.map
